dff4119486027e39001cb3f582cdac0eef24409b
Mark <70436486+Garmonik@users.noreply.github.com>
2021-04-10 02:21:56 +0300

    Update Bubble_Sort.c

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -3,28 +3,28 @@
 
 int main()
 {
-    int N;
+    int size;
     printf("Input N: ");
-    scanf("%d", &N);
-    int* mass;
-    mass = (int *)malloc(N * sizeof(int));
+    scanf("%d", &size);
+    int* array;
+    array = (int *)malloc(size * sizeof(int));
     printf("Input the array elements:\n");
-    for (int i = 0; i < N; i++)
-        scanf("%d", &mass[i]);
+    for (int i = 0; i < size; i++)
+        scanf("%d", &array[i]);
     //сортировка пузырьком
     int tmp;
     bool noSwap;
 
-    for (int i = N - 1; i >= 0; i--)
+    for (int i = size - 1; i >= 0; i--)
     {
         noSwap = 1;
         for (int j = 0; j < i; j++)
         {
-            if (mass[j] > mass[j + 1])
+            if (array[j] > array[j + 1])
             {
-                tmp = mass[j];
-                mass[j] = mass[j + 1];
-                mass[j + 1] = tmp;
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
                 noSwap = 0;
             }
         }
@@ -32,9 +32,9 @@ int main()
             break;
     }
     printf("Sorted array:\n");
-    for (int i = 0; i < N; i++)
-        printf("%d ", mass[i]);
+    for (int i = 0; i < size; i++)
+        printf("%d ", array[i]);
     printf("\n");
-    free(mass);
+    free(array);
     return 0;
 }
